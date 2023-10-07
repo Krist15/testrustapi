762b8ce0b0948b5d11c2f0cae516dbb3e2784616
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-10-07 02:16:43 -0400

    .

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv;
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+#[derive(Deserialize, Serialize)]
+struct User {
+    name: String,
+    email: String,
+}
+
+#[get("/ping")]
+async fn ping(data: web::Data<AppState>) -> impl Responder {
+    let query_result = sqlx::query_as!(
+        User,
+        "SELECT name, email FROM users WHERE email = 'admin@admin.com'"
+    )
+    .fetch_one(&data.db)
+    .await
+    .unwrap();
+
+    HttpResponse::Ok().json(query_result)
+}
+
+struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(std::env::var("DATABASE_URL").unwrap().as_str())
+        .await
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            .service(ping)
+    })
+    .bind(("127.0.0.1", 18200))?
+    .run()
+    .await
+}
